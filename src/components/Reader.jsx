2e4b52453258022e43b35148ebef07ea72dbfd82
Busa Balázs <busa.balazs@intra.echotv.hu>
2025-07-02 08:30:54 +0200

    change the qr scenner api

--- src/components/Reader.jsx
+++ src/components/Reader.jsx
@@ -1,71 +1,34 @@
-import React, { useEffect } from "react";
-import { Html5QrcodeScanner, Html5QrcodeScanType } from "html5-qrcode";
-
-//----------------------------------------------------------------------
-//----------------------------------------------------------------------
-const CONFIG = {
-  qrbox: { width: 500, height: 800 },
-  fps: 5,
-  videoConstraints: {
-    facingMode: { exact: "environment" },
-  },
-  rememberLastUsedCamera: true,
-  supportedScanTypes: [Html5QrcodeScanType.SCAN_TYPE_CAMERA],
-};
-
-//----------------------------------------------------------------------
-//----------------------------------------------------------------------
+import React, { useEffect, useRef } from "react";
+import QrScanner from "qr-scanner";
 
 const Reader = ({ scanId }) => {
+  const videoRef = useRef(null);
+  const scannerRef = useRef(null);
+  
   useEffect(() => {
-    const scanner = new Html5QrcodeScanner("reader", CONFIG);
-
-    scanner.render(success, error);
-
-    function success(result) {
-      scanner.clear();
-      scanId(result); // triger this function to Question.jsx throught the Modal.jsx
-    }
-
-    function error(err) {
-      console.warn(err);
-    }
-
-     // Change button text after the scanner is rendered
-     const interval = setInterval(() => {
-      const requestCamerButton = document.querySelector(
-        "#html5-qrcode-button-camera-permission"
+    if (videoRef.current) {
+      scannerRef.current = new QrScanner(
+        videoRef.current,
+        (result) => {
+          scanId(result.data);
+          scannerRef.current.stop();
+        },
+        {
+          preferredCamera: "environment",
+        }
       );
-      const startButton = document.querySelector("#html5-qrcode-button-camera-start");
-      const stopButton = document.querySelector("#html5-qrcode-button-camera-stop");
-
-      if (requestCamerButton) {
-        requestCamerButton.textContent = "Kamera Engedélyezése";
-
-        clearInterval(interval);
-      }
-
-      if (startButton) {
-        startButton.textContent = "Beolvasás Kezdés";
-        clearInterval(interval);
-      }
-
-      if (stopButton) {
-        stopButton.textContent = "Beolvasás Megállítás";
-        clearInterval(interval);
-      }
-    }, 100);
-
+      scannerRef.current.start();
+    }
+    
     return () => {
-      scanner.clear();
-      clearInterval(interval); // Cleanup interval on component unmount
+      scannerRef.current?.stop();
     };
-  }, []);
-
+  }, [scanId]);
+  
   return (
-    <>
-      <div id="reader"></div>
-    </>
+    <div>
+    <video ref={videoRef} style={{ width: "100%", maxWidth: 500 }} />
+    </div>
   );
 };
 
